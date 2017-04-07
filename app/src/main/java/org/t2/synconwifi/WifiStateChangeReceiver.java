a2fb824fbf28ba2696f169544d890a1f49cb4acf
Kadabash <t2dev@outlook.com>
2017-04-07 13:50:18 +0200

    Add Broadcast Receiver for Wifi state changes

---
+++ app/src/main/java/org/t2/synconwifi/WifiStateChangeReceiver.java
@@ -0,0 +1,24 @@
+package org.t2.synconwifi;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class WifiStateChangeReceiver extends BroadcastReceiver {
+    private static final String TAG = "WifiStateChangeReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //TODO: Implement onReceive() in WifiStateChangedReceiver.
+
+        //DEBUG:
+        StringBuilder sb = new StringBuilder();
+        sb.append("Action: " + intent.getAction() + "\n");
+        sb.append("URI: " + intent.toUri(Intent.URI_INTENT_SCHEME).toString() + "\n");
+        String log = sb.toString();
+        Log.d(TAG, log);
+        Toast.makeText(context, log, Toast.LENGTH_LONG).show();
+    }
+}
