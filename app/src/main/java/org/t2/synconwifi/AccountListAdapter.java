400c74d73958bd809b1ddb84323222fe967e8571
Kadabash <t2dev@outlook.com>
2017-04-07 16:46:56 +0200

    Make changes to the list of active accounts permanent.

--- app/src/main/java/org/t2/synconwifi/AccountListAdapter.java
+++ app/src/main/java/org/t2/synconwifi/AccountListAdapter.java
@@ -2,16 +2,17 @@ package org.t2.synconwifi;
 
 import android.accounts.Account;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
-import android.widget.CheckedTextView;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Set;
 
 /**
  * Created by t2 on 07.04.17.
@@ -51,18 +52,42 @@ public class AccountListAdapter extends ArrayAdapter<Account> {
             holder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    //TODO: Implement saving of checked list items in shared preferences
+                    SharedPreferences sharedPreferences = mContext.getSharedPreferences("AccountsActive", Context.MODE_PRIVATE);
+                    Account associatedAccount = (Account) buttonView.getTag();
+                    String accountTypeAndName = associatedAccount.type + ";" + associatedAccount.name;     //Accounts are identified in AccountsActive preferences by their type and name, separated by a ";".
+                    boolean isActiveAccount = sharedPreferences.getBoolean(accountTypeAndName, false);
+                    if(isActiveAccount) {
+                        if(!isChecked) {
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putBoolean(accountTypeAndName, false);        //Set account inactive.
+                            editor.commit();
+                        }
+                    } else {
+                        if(isChecked) {
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putBoolean(accountTypeAndName, true);       //Set account active.
+                            editor.commit();
+                        }
+                    }
+
                 }
             });
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
 
+        //Assign account to list item:
         Account account = accountList.get(position);
+        holder.checkBox.setTag(account);
         holder.textViewAccountName.setText(account.name);
         holder.textViewAccountType.setText(account.type);
         holder.textViewAccountName.setTag(account);
         holder.textViewAccountType.setTag(account);
+
+        //Set checked state from shared preferences:
+        SharedPreferences sharedPreferences = mContext.getSharedPreferences("AccountsActive", Context.MODE_PRIVATE);
+        String accountTypeAndName = account.type + ";" + account.name;     //Accounts are identified in AccountsActive preferences by their type and name, separated by a ";".
+        holder.checkBox.setChecked(sharedPreferences.getBoolean(accountTypeAndName, false));
 
         return convertView;
     }
