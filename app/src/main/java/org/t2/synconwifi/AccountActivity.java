143892cc7e82f8a87d0a0958ca79a8bb143ed5c3
Kadabash <t2dev@outlook.com>
2017-04-08 18:37:30 +0200

    Main UI. Not pretty but functional.

---
+++ app/src/main/java/org/t2/synconwifi/AccountActivity.java
@@ -0,0 +1,67 @@
+package org.t2.synconwifi;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AccountActivity extends AppCompatActivity {
+    public static final int PERMISSION_GRANTED_GET_CONTACTS = 1;
+    public static final int PERMISSION_GRANTED_WRITE_SYNC_SETTIGNS = 2;
+    private AccountListAdapter listAdapter = null;
+
+    private ListView accountListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+
+        //Create checkbox list to select accounts to control:
+        this.accountListView = (ListView) findViewById(R.id.accountList);
+
+        //Ask for runtime permissions:
+        int permissionCheckGetAccounts = ContextCompat.checkSelfPermission(this, android.Manifest.permission.GET_ACCOUNTS);
+        if(permissionCheckGetAccounts != PackageManager.PERMISSION_GRANTED) {
+            //TODO: explain why the permission is needed (as in https://developer.android.com/training/permissions/requesting.html#perm-request)
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.GET_ACCOUNTS}, PERMISSION_GRANTED_GET_CONTACTS);
+        } else {
+            setUpListView();
+        }
+    }
+
+    private void setUpListView() {
+        //Get Accounts:
+        AccountManager accountManager = AccountManager.get(this);
+        Account[] accounts = accountManager.getAccounts();
+        ArrayList<Account> accountArrayList = new ArrayList<>(Arrays.asList(accounts));
+
+        //Set list adapter and add list to layout:
+        this.listAdapter = new AccountListAdapter(this, R.layout.account_list_item, accountArrayList);
+        this.accountListView.setAdapter(listAdapter);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_GRANTED_GET_CONTACTS: {
+                //If permission has been granted:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    setUpListView();
+                } else {
+                    //If permission has NOT been granted:
+                    Toast.makeText(this, "This app will not function without permission to read accounts. Please open the app again and grant it.", Toast.LENGTH_LONG).show(); //TODO: Internationalise string
+                }
+            } break;
+        }
+    }
+}
--- app/src/main/java/org/t2/synconwifi/SettingsActivity.java
+++ app/src/main/java/org/t2/synconwifi/SettingsActivity.java
@@ -2,6 +2,7 @@ package org.t2.synconwifi;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
@@ -11,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -23,11 +26,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SettingsActivity extends AppCompatActivity {
-    public static final int PERMISSION_GRANTED_GET_CONTACTS = 1;
-    public static final int PERMISSION_GRANTED_WRITE_SYNC_SETTIGNS = 2;
-    private AccountListAdapter listAdapter = null;
-
-    private ListView accountListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,17 +54,15 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
-        //Create checkbox list to select accounts to control:
-        this.accountListView = (ListView) findViewById(R.id.accountSettingListView);
-
-        //Ask for runtime permissions:
-        int permissionCheckGetAccounts = ContextCompat.checkSelfPermission(this, android.Manifest.permission.GET_ACCOUNTS);
-        if(permissionCheckGetAccounts != PackageManager.PERMISSION_GRANTED) {
-            //TODO: explain why the permission is needed (as in https://developer.android.com/training/permissions/requesting.html#perm-request)
-            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.GET_ACCOUNTS}, PERMISSION_GRANTED_GET_CONTACTS);
-        } else {
-            setUpListView();
-        }
+        //Add button to change to AccountActivity:
+        Button accountActivityButton = (Button) findViewById(R.id.accountSettingsButton);
+        accountActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AccountActivity.class);
+                startActivity(intent);
+            }
+        });
 
         //Add an input field for Wifi SSID:
         EditText editTextSSID = (EditText) findViewById(R.id.ssidSettingEditText);
@@ -91,31 +87,5 @@ public class SettingsActivity extends AppCompatActivity {
                 editor.apply();
             }
         });
-    }
-
-    private void setUpListView() {
-        //Get Accounts:
-        AccountManager accountManager = AccountManager.get(this);
-        Account[] accounts = accountManager.getAccounts();
-        ArrayList<Account> accountArrayList = new ArrayList<>(Arrays.asList(accounts));
-
-        //Set list adapter and add list to layout:
-        this.listAdapter = new AccountListAdapter(this, R.layout.account_list_item, accountArrayList);
-        this.accountListView.setAdapter(listAdapter);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_GRANTED_GET_CONTACTS: {
-                //If permission has been granted:
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    setUpListView();
-                } else {
-                    //If permission has NOT been granted:
-                    Toast.makeText(this, "This app will not function without permission to read accounts. Please open the app again and grant it.", Toast.LENGTH_LONG).show(); //TODO: Internationalise string
-                }
-            } break;
-        }
     }
 }
